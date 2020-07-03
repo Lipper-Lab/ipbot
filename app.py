47ac8f95ddbe0ad7eba3930da7d6477d8a911d3a
Lance Suen <10292888+LanceSuen@users.noreply.github.com>
2020-07-03 17:37:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import request
 from flask import jsonify
 
+app = Flask(__name__)
+
 @app.route("/", methods=["GET"])
 def get_my_ip():
     return jsonify({'ip': request.remote_addr}), 200
-
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
