435ead89200bb8a972f759912e1f5cd9df42aff1
Lance Suen <10292888+LanceSuen@users.noreply.github.com>
2020-07-03 17:58:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def get_my_ip():
+@app.route("/j", methods=["GET"])
+def getJSON():
     if request.headers.getlist("X-Forwarded-For"):
         ip = request.headers.getlist("X-Forwarded-For")[0]
     else:
         ip = request.remote_addr
     return jsonify({'ip': ip}), 200
+
+@app.route("/", methods=["GET"])
+def getIP():
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = request.remote_addr
+    return str(ip), 200
+    
+    
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
